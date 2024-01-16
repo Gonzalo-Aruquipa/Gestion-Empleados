6504640ea187f237c70ca45c8dd5abaa743ab8f9
Gonzalo Aruquipa <gonzao777info@gmail.com>
2024-01-16 17:23:53 -0400

    department´s crud

--- controllers/departmentController.js
+++ controllers/departmentController.js
@@ -4,7 +4,44 @@ exports.newDepartment = async (req, res) => {
   const department = new Department(req.body);
   try {
     await department.save();
-    res.status(200).send({ message: "Department created Successfully" });
+    res.status(200).send("Department created Successfully");
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.getIdDepartment = async (req, res, next) => {
+  const department = await Department.findById(req.params.id);
+  try {
+    if (!department) {
+      throw new Error("department not found");
+    }
+    res.status(200).json(department);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+
+exports.updateDepartment = async (req, res) => {
+  try {
+    let department = await Department.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      {
+        new: true,
+      }
+    );
+    res.status(200).json(department);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
+
+exports.deleteDepartment = async (req, res) => {
+  try {
+    await Department.findByIdAndDelete(req.params.id);
+    res.status(200).send("Department delete successfully");
   } catch (error) {
     res.status(400).send(error.message);
   }
--- routes/departmentRoutes.js
+++ routes/departmentRoutes.js
@@ -3,6 +3,9 @@ const departmentController = require("../controllers/departmentController");
 
 const router = Router();
 
-router.post("/newdep", departmentController.newDepartment)
+router.post("/newdep", departmentController.newDepartment);
+router.get("/:id", departmentController.getIdDepartment);
+router.put("/:id", departmentController.updateDepartment);
+router.delete("/:id", departmentController.deleteDepartment);
 
 module.exports = router;
